05d2b1c85b45c228ed6a520bc47e0734c61b9711
Jerome Santos <msantos@luc.edu>
2021-12-15 00:08:59 -0600

    attempt to connect to mongodb

--- script.js
+++ script.js
@@ -37,22 +37,49 @@ for (var i = 0; i < accordions.length; i++) {
   }
 }
 
-//database
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+//attempt to connect top database
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
 
+const app = express()
 
-app.use(bodyParser.urlencoded)({extendee: true});
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 
+mongoose.connect('mongodb+srv://test123:test123@markited.t35fs.mongodb.net/data1',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-app.get("/", function(req, res){
-  res.send("express is working")
-})
+var db = mongoose.connection;
 
-//app.post
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
 
-app.listen(3000, function(){
-  console.log("running..");
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var password = req.body.password;
+
+    var data = {
+        "name": name,
+        "email" : email,
+        "phno": phno,
+        "password" : password
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signup_success.html')
+
 })
