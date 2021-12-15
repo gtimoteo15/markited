b93cb0bfbaec91630f138b0bcd014b8131325500
Jerome Santos <msantos@luc.edu>
2021-12-14 19:56:41 -0600

    update

--- script.js
+++ script.js
@@ -34,3 +34,22 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+//database
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded)({extendee: true});
+
+
+app.get("/", function(req, res){
+  res.send("express is working")
+})
+
+//app.post
+
+app.listen(3000, function(){
+  console.log("running..");
+})
